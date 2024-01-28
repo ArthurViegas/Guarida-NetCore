09ce452ab402dc632e60af436047c0014177999f
Arthur Viegas <viegas.arthur2013@hotmail.com>
2024-01-28 20:03:50 -0300

    feat: new AdvacnedLists project with method that order a string list by its frequency

---
+++ AdvancedLists/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Program
+{
+    using System;
+    public static class AdvancedListUtils
+    {
+        public static void Main()
+        {
+            List<string> originalList = ["maçã", "banana", "banana", "laranja", "banana", "maçã", "banana"];
+            
+            ImprimirLista(originalList, "Lista Original");
+
+            var orderedList = OrdenarPorFrequencia(originalList);
+
+            ImprimirLista(orderedList, "Lista Ordenada");
+        }
+
+        public static List<string> OrdenarPorFrequencia(List<string> lista)
+        {
+            return lista.GroupBy(x => x)
+                                  .OrderByDescending(y => y.Count())
+                                  .Select(y => y.Key)
+                                  .ToList();
+        }
+
+        static void ImprimirLista(List<string> list, string description)
+        {
+            Console.WriteLine($"{description}: ");
+
+            foreach (var elemento in list)
+            {
+                Console.Write($"{elemento} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
